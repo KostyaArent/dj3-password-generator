7ed3e62d1e1b0ca598ca1e6318c4fdeaa9e7ffc5
KostyaArent <trepalin.konstantin@gmail.com>
2022-03-28 06:14:36 +0000

    test commit

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import random
 
-# Create your views here.
+
 def home(request):
     return render(request, 'generator/home.html')
 
